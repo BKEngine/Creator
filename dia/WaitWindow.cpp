47d9be2fd9afd21cef8f65213bc620d1579a569c
gjy <gjy@9eef8c3c-8722-4a17-8556-dca5b306aafa>
2015-03-25 07:25:15 +0000

    git-svn-id: svn://icemaple.info:8888/bkecreator@102 9eef8c3c-8722-4a17-8556-dca5b306aafa

---
+++ dia/WaitWindow.cpp
@@ -0,0 +1,40 @@
+#include <weh.h>
+
+#include "WaitWindow.h"
+#include "ui_WaitWindow.h"
+
+WaitWindow::WaitWindow(QWidget *parent)
+	:QDialog(parent),
+	ui(new Ui::WaitWindow)
+{
+	ui->setupUi(this);
+}
+
+WaitWindow::~WaitWindow()
+{
+	delete ui;
+}
+
+void WaitWindow::setInfo(const QString &s, int max)
+{
+	in = s;
+	m_max = max;
+	ui->pb->setMaximum(m_max);
+	setNum(1);
+}
+
+void WaitWindow::setNum(int n)
+{
+	m_cur = n;
+	QString tmp, tmp2;
+	tmp.setNum(m_max, 10);
+	tmp2.setNum(m_cur, 10);
+	ui->info->setText(in + "\t" + tmp2 + "/" + tmp);
+	ui->pb->setValue(n);
+	update();
+}
+
+void WaitWindow::addNum()
+{
+	setNum(m_cur + 1);
+}
---
+++ dia/WaitWindow.h
@@ -0,0 +1,29 @@
+#include <weh.h>
+
+#include <QDialog>
+
+namespace Ui {
+	class WaitWindow;
+}
+
+class WaitWindow :public QDialog
+{
+	Q_OBJECT
+public:
+	WaitWindow(QWidget *parent = 0);
+
+	~WaitWindow();
+
+	void setInfo(const QString &s, int max);
+
+	void setNum(int n);
+
+	void addNum();
+
+private:
+	Ui::WaitWindow *ui;
+
+	int m_cur;
+	int m_max;
+	QString in;
+};
